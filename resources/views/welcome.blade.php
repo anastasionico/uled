604c2a8a02c18e9b571d212a6cb89a9fe1ff923a
anastasionico <nico@anastasionico.uk>
2019-10-02 23:54:27 +0100

    add simple page layout

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Uniled Holding Text</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+
+<body>
+    <main id="root">
+        <section class="banner">
+            <img src="{{ asset('logo.svg') }}"/>
+            <div class="clearfix"></div>
+        </section>
+        <section class="content">
+            <header class="content-header">
+                <h1>Uniled Holding Text</h1>
+                <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum neque egestas congue quisque egestas diam in.</h6>
+            </header>
+            <article class="content-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis lectus magna fringilla urna porttitor. Id velit ut tortor pretium viverra. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Rutrum quisque non tellus orci ac auctor augue mauris. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Netus et malesuada fames ac turpis egestas maecenas pharetra. Est lorem ipsum dolor sit. Urna porttitor rhoncus dolor purus non enim praesent elementum facilisis. <a href="#">click here</a> </p>
+                
+                <p>Sed lectus vestibulum mattis ullamcorper. Sed euismod nisi porta lorem mollis. Quis ipsum suspendisse ultrices gravida dictum fusce. Aliquam sem fringilla ut morbi tincidunt augue interdum velit. Nisi est sit amet facilisis magna etiam tempor. In egestas erat imperdiet sed. Nibh ipsum consequat nisl vel pretium. In mollis nunc sed id semper risus in. Malesuada fames ac turpis egestas. Eget egestas purus viverra accumsan in. Mauris augue neque gravida in fermentum et sollicitudin ac. Neque aliquam vestibulum morbi blandit cursus risus at.</p>
+                
+                <p>Lorem mollis aliquam ut porttitor. Lacinia quis vel eros donec. Sed velit dignissim sodales ut eu sem integer vitae justo. Facilisis volutpat est velit egestas dui id ornare arcu. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Semper risus in hendrerit gravida rutrum quisque non. Quisque non tellus orci ac auctor augue mauris. Gravida neque convallis a cras semper. Faucibus turpis in eu mi bibendum neque egestas congue. Sed risus ultricies tristique nulla aliquet enim tortor at. Vel turpis nunc eget lorem dolor sed viverra ipsum. Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Egestas congue quisque egestas diam in arcu cursus.Ridiculus mus mauris vitae ultricies leo integer malesuada nunc. Morbi blandit cursus risus at. </p>
+                
+                <p>Quis imperdiet massa tincidunt nunc pulvinar sapien et. Velit ut tortor pretium viverra suspendisse potenti nullam. Consectetur a erat nam at lectus urna duis convallis convallis. Nisi porta lorem mollis aliquam. Ullamcorper sit amet risus nullam eget. Quis enim lobortis scelerisque fermentum dui faucibus in ornare quam. Sed risus pretium quam vulputate dignissim suspendisse. Tristique senectus et netus et malesuada fames. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Eget mi proin sed libero enim. Quam quisque id diam vel quam elementum pulvinar etiam non. Eu non diam phasellus vestibulum lorem. Pharetra vel turpis nunc eget lorem. Nibh ipsum consequat nisl vel pretium. In mollis nunc sed id semper risus in. Malesuada fames ac turpis egestas. Eget egestas purus viverra accumsan in. Mauris augue neque gravida in fermentum et sollicitudin ac. Neque aliquam vestibulum morbi blandit cursus risus at.</p>
+            </article>
+            <footer class="content-footer">
+                <p>
+                    READ MORE ABOUT HOW WE WORK 
+                    <span>
+                        &#x27A1;
+                    </span>
+                </p>
+                <div class="clearfix"></div>
+            </footer>
+        </section>
+        <footer class="links">
+            <nav>
+                <ul>
+                    <li>&copy; Uniled 2017</li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Condition</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </footer>
+    </main>
+</body>
+</html>
